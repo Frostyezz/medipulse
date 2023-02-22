1a3cd4b200c8e6db65b7318934cd7ab355cb9165
Robert Moraru <robert.moraru2012@yahoo.com>
2023-02-22 19:54:54 +0200

    +mantine +configs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,36 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
+import { Inter } from "@next/font/google";
+import RouterTransition from "@/components/common/RouterTransition";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const inter = Inter({ subsets: ["latin"] });
+
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>MediPulse</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          fontFamily: inter.style.fontFamily,
+          colorScheme: "light",
+        }}
+      >
+        <RouterTransition />
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
