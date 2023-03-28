833a81f189dcba951a03c0b11fe644c9631dc68d
Robert Moraru <robert.moraru2012@yahoo.com>
2023-03-28 14:33:21 +0300

    +register redirect fix

--- src/views/Register/index.tsx
+++ src/views/Register/index.tsx
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useMemo } from "react";
 import CreateAccount from "./CreateAccount";
 import { useAppSelector } from "@/services/redux/hooks";
 import VerifyEmail from "./VerifyEmail";
 import Redirect from "@/common/components/Redirect";
 import { ROUTES } from "@/common/utils/routes";
 import CreateProfile from "./CreateProfile/CreateProfile";
+import { ROLES } from "@/services/graphql/types/enums";
 
-const RegisterControllerComponents: Record<number, React.ReactElement> = {
-  0: <CreateAccount />,
-  1: <VerifyEmail />,
-  2: <CreateProfile />,
-  3: <Redirect to={ROUTES.ROOT} replace />,
+const RedirectPath: Record<ROLES, ROUTES> = {
+  [ROLES.MEDIC]: ROUTES.MEDIC_DASHBOARD,
+  [ROLES.NURSE]: ROUTES.NURSE_DASHBOARD,
+  [ROLES.PATIENT]: ROUTES.PATIENT_DASHBOARD,
 };
 
 const RegisterController: React.FC = () => {
-  const registerStep = useAppSelector((store) => store.user?.registerStep);
+  const { registerStep, role } = useAppSelector((store) => store.user) ?? {};
+
+  const RegisterControllerComponents = useMemo<
+    Record<number, React.ReactElement>
+  >(
+    () => ({
+      0: <CreateAccount />,
+      1: <VerifyEmail />,
+      2: <CreateProfile />,
+      3: <Redirect to={ROUTES[`${role ?? ROLES.MEDIC}_DASHBOARD`]} replace />,
+    }),
+    [role]
+  );
   return RegisterControllerComponents[registerStep ?? 0];
 };
 
